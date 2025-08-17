240064a50f8af46706f7fa9373eadf3564308bb1
Jerry Zhang <zhang.jiahe@gmail.com>
2025-08-16 21:51:19 -0400

    Add files via upload
    Inserts dummy data into the DRUG_DISCOVERY table to test the Fivetran data pipeline.

---
+++ Dummy Data Insertion.sql
@@ -0,0 +1,44 @@
+-- Generate dummy data to test the Fivetran interface
+INSERT INTO drug_discovery (
+    compound_id,
+    protein_id,
+    molecular_weight,
+    logp,
+    h_bond_donors,
+    h_bond_acceptors,
+    rotatable_bonds,
+    polar_surface_area,
+    compound_clogp,
+    protein_length,
+    protein_pi,
+    hydrophobicity,
+    binding_site_size,
+    mw_ratio,
+    logp_pi_interaction,
+    binding_affinity,
+    active
+)
+VALUES (
+    'CID_TEST: ' || SUBSTR(CURRENT_TIMESTAMP, 1, 15),
+    'PID_TEST: ' || SUBSTR(CURRENT_TIMESTAMP, 1, 15),
+    284.243,
+    1.312,
+    2,
+    8,
+    5,
+    153.088,
+    0.855,
+    141,
+    3.847,
+    0.98,
+    8.865,
+    0.312,
+    21.181,
+    9.496,
+    1
+);
+
+-- Validate dummy data insertion
+SELECT *
+FROM drug_discovery
+WHERE compound_id LIKE 'CID_TEST%';
