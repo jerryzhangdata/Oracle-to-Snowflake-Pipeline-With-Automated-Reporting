b9a1c041ad6f6956ed2e35e20022cb74f740b321
Jerry Zhang <zhang.jiahe@gmail.com>
2025-08-26 19:05:19 -0700

    Update Fivetran Setup.sql

--- Oracle SQL/Fivetran Setup.sql
+++ Oracle SQL/Fivetran Setup.sql
@@ -1,6 +1,6 @@
 -- Configured per fivetran setup guide: https://fivetran.com/docs/connectors/databases/oracle/oracle-connector/setup-guide
 -- The Admin user has insufficient privileges to grant access to DBA_SEGMENTS
-CREATE USER fivetran_user IDENTIFIED BY "!9uGLU#aCwgTRJy";
+CREATE USER fivetran_user IDENTIFIED BY "(Password, Redacted)";
 GRANT SELECT ON DRUG_DISCOVERY TO fivetran_user;
 GRANT SELECT ON DBA_EXTENTS TO fivetran_user;
 GRANT SELECT ON DBA_TABLESPACES TO fivetran_user;
